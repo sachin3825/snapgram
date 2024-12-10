61092d85249705280c74eddd722eb5fc882c16ea
Sachin kumawat <sachinkumawat55@gmail.com>
2024-12-10 21:28:59 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,29 @@
+import { Routes, Route } from "react-router-dom";
+import SigninForm from "./_auth/forms/SIgninForm";
+import SignupForm from "./_auth/forms/SignupForm";
+import AuthLayout from "./_auth/AuthLayout";
+import RootLayout from "./_root/RootLayout";
+import { Home } from "./_root/pages";
+
+import "./globals.css";
+
+const App = () => {
+  return (
+    <main className="flex h-screen">
+      <Routes>
+        {/* Public Routes */}
+        <Route element={<AuthLayout />}>
+          <Route path="/sign-in" element={<SigninForm />} />
+          <Route path="/sign-up" element={<SignupForm />} />
+        </Route>
+
+        {/* Private Routes */}
+        <Route element={<RootLayout />}>
+          <Route index element={<Home />} />
+        </Route>
+      </Routes>
+    </main>
+  );
+};
+
+export default App;
---
+++ src/_auth/forms/SigninForm.tsx
@@ -0,0 +1,5 @@
+const SigninForm = () => {
+  return <div>SIgninForm</div>;
+};
+
+export default SigninForm;
